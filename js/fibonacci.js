27765b35dcf16955b2f02cdd7518069fa4aa228f
KevinBaoB <kevinbaobui@gmail.com>
2022-05-25 09:39:09 -0500

    First

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,15 @@
 const fibonacci = (num) => {
-    
-}
+  if (num === 0 || num === 1) return num;
 
-module.exports = {fibonacci}
+  let first = 0;
+  let second = 1;
+  sum = first + second;
+  for (let i = 2; i < num; i++) {
+    first = second;
+    second = sum;
+    sum = first + second;
+  }
+  return sum;
+};
+
+module.exports = { fibonacci };
