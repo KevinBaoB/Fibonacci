f5a979871f1928aee1a88fa5c9470ab9418d315e
KevinBaoB <kevinbaobui@gmail.com>
2022-05-25 12:11:49 -0500

    First

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,17 @@
 def fibonacci(n):
-  pass
+  if n <2:
+     return n
+
+  start = [0,1]
+
+  for i in range(n):
+    start.append(start[-1] + start[-2])
+    
+  return start[n]
+
+
+print(fibonacci(8))
+
+  # pass
 
 
